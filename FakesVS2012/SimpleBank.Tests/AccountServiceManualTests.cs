e9b58ed124081d5e64447a9fc80999a771d2a73e
hoonzis@gmail.com <hoonzis@gmail.com>
2012-09-27 23:22:41 +0200

    rewriting pex to manual tests

--- FakesVS2012/SimpleBank.Tests/AccountServiceManualTests.cs
+++ FakesVS2012/SimpleBank.Tests/AccountServiceManualTests.cs
@@ -2,59 +2,68 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SimpleBank.Domain;
 using SimpleBank.Repository.Fakes;
 
 namespace SimpleBank.Tests
 {
+    [TestClass]
     public class AccountServiceManualTests
     {
-        public void TestMakeTransfer(Account creditAccount, Account debitAccount, decimal amount)
+        private List<Account> _accounts = new List<Account>();
+
+        [TestInitialize]
+        public void setUp()
         {
-
-            StubIAccountRepository accountRepository = new StubIAccountRepository();
-            StubIOperationRepository operationRepository = new StubIOperationRepository();
-            AccountService service = new AccountService(accountRepository, operationRepository);
-            service.MakeTransfer(creditAccount, debitAccount, amount);
-           
-        }
-
-        public decimal ComputeInterest(Account account, double annualRate, int months)
-        {
-
-            StubIAccountRepository accountRepository = new StubIAccountRepository();
-            StubIOperationRepository operationRepository = new StubIOperationRepository();
-
-            AccountService service = new AccountService(accountRepository, operationRepository);
-
-            decimal result = service.ComputeInterest(account, annualRate, months);
-
-            return result;
-        }
-
-        public List<Operation> GetOperationsForAccount(int accountID)
-        {
-            
             List<Operation> operations1 = new List<Operation>();
             operations1.Add(new Operation { Amount = 100, Direction = Domain.Direction.Credit });
             operations1.Add(new Operation { Amount = 200, Direction = Domain.Direction.Debit });
 
 
-            List<Account> accounts = new List<Account>();
-            accounts.Add(new Account { Balance = 300, Operations = operations1, AutorizeOverdraft = true, Id = 1 });
-            accounts.Add(new Account { Balance = 0, Operations = null, AutorizeOverdraft = false, Id = 2 });
+            
+            _accounts.Add(new Account { Balance = 300, Operations = operations1, AutorizeOverdraft = true, Id = 1 });
+            _accounts.Add(new Account { Balance = 0, Operations = null, AutorizeOverdraft = false, Id = 2 });
+        }
 
+        [TestMethod]
+        public void TestMakeTransfer_debit()
+        {
+
+            StubIAccountRepository accountRepository = new StubIAccountRepository();
+            StubIOperationRepository operationRepository = new StubIOperationRepository();
+            AccountService service = new AccountService(accountRepository, operationRepository);
+            service.MakeTransfer(_accounts[0], _accounts[1], 200);
+        }
+
+        [TestMethod]
+        public void ComputeInterest()
+        {
+
+            //StubIAccountRepository accountRepository = new StubIAccountRepository();
+            //StubIOperationRepository operationRepository = new StubIOperationRepository();
+
+            AccountService service = new AccountService(null, null);
+
+            decimal result = service.ComputeInterest(new Account { Balance = 350 }, 0.1, 20);
+
+            Assert.AreEqual(result, 123);
+        }
+
+        [TestMethod]
+        public void GetOperationsForAccount_AccountFound()
+        {
             StubIAccountRepository accountRepository = new StubIAccountRepository();
             accountRepository.GetAccountInt32 = (x) =>
             {
-                return accounts.SingleOrDefault(a => a.Id == x);
+                return _accounts.SingleOrDefault(a => a.Id == x);
             };
 
             StubIOperationRepository operationRepository = new StubIOperationRepository();
             AccountService service = new AccountService(accountRepository, operationRepository);
 
-            List<Operation> result = service.GetOperationsForAccount(accountID);
-            return result;            
+            List<Operation> result = service.GetOperationsForAccount(1);
+            Assert.AreEqual(result.Count, 2);
         }
     }
 }
---
+++ FakesVS2012/SimpleBank.Tests/UtilsManualTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleBank.Tests
+{
+    [TestClass]
+    public class UtilsManualTests
+    {
+
+        [TestMethod]
+        public int Fibonacci(int x)
+        {
+            int result = Utils.Fibonacci(x);
+            return result;
+            // TODO: add assertions to method UtilsTest.Fibonacci(Int32)
+        }
+
+
+         [TestMethod]
+        public string ReadFooValue()
+        {
+            using (ShimsContext.Create())
+            {
+                System.IO.Fakes.ShimFile.ReadAllTextString = (x) =>
+                {
+                    return "Hello World";
+                };
+
+                System.IO.Fakes.ShimFile.ExistsString = (x) => true;
+
+                string result = Utils.ReadFooValue();
+
+                return result;
+            }
+        }
+
+         [TestMethod]
+        public string SomeDumpMethod(int i, int j)
+        {
+            string result = Utils.SomeDumbMethod(i, j);
+            return result;
+
+        }
+         [TestMethod]
+        public string Capitalize(string parametr)
+        {
+            using (ShimsContext.Create())
+            {
+                System.IO.Fakes.ShimFile.ReadAllTextString = (x) =>
+                {
+                    return parametr;
+                };
+
+                System.IO.Fakes.ShimFile.ExistsString = (x) => true;
+
+                string result = Utils.Capitalize();
+                return result;
+                // TODO: add assertions to method UtilsTest.Capitalize()
+            }
+        }
+
+        [TestMethod]
+        public string GetMessage(bool newyear)
+        {
+
+            using (ShimsContext.Create())
+            {
+                System.Fakes.ShimDateTime.NowGet =
+                    () =>
+                    {
+                        if (newyear)
+                        {
+                            return new DateTime(1, 1, 1);
+                        }
+                        return new DateTime(2, 2, 2);
+                    };
+            }
+
+            string result = Utils.GetMessage();
+            return result;
+        }
+    }
+}
